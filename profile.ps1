caed21de1d0a8135b9611d7f69c23bccf7ab58a7
geloman <geloman-likes-rust@users.noreply.github.com>
2024-03-26 03:36:53 +0800

    Add profile file

---
+++ profile.ps1
@@ -0,0 +1,13 @@
+$Env:Path += ";$HOME\dotfiles\handy-scripts\bin"
+
+Set-Alias -Name vi -Value nvim
+Set-Alias -Name vim -Value nvim
+Set-Alias -Name ff -Value find-files
+Set-Alias -Name fcd -Value fast-cd
+Set-Alias -Name vr -Value vim-recent
+Set-Alias -Name drt -Value dirty-files
+Set-Alias -Name gs -Value git-status
+Set-Alias -Name gf -Value git-files
+Set-Alias -Name ga -Value git-stage-file
+Set-Alias -Name gu -Value git-unstage-file
+Set-Alias -Name lg -Value live-grep
