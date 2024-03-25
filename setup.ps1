8cb5cdc5c7419dd9a1ccd97ab3a0e3bbdf2a3fa5
geloman <geloman-likes-rust@users.noreply.github.com>
2024-03-26 03:37:04 +0800

    Create setup script

---
+++ setup.ps1
@@ -0,0 +1,15 @@
+Get-Content $HOME\dotfiles\profile.ps1 > $PROFILE
+
+if (Test-Path -Path $HOME\.gitconfig) {
+    Move-Item -Path $HOME\.gitconfig -Destination $HOME\.gitconfig.bak
+}
+New-Item -ItemType SymbolicLink -Path $HOME\.gitconfig -Target $HOME\dotfiles\.gitconfig 
+
+if (Test-Path -Path $Env:LOCALAPPDATA\nvim) {
+    Move-Item -Path $Env:LOCALAPPDATA\nvim -Destination $Env:LOCALAPPDATA\nvim.bak
+}
+New-Item -ItemType SymbolicLink -Path $Env:LOCALAPPDATA\nvim -Target $HOME\dotfiles\nvim 
+
+winget install --accept-source-agreements --id=Neovim.Neovim --exact
+winget install --accept-source-agreements chocolatey.chocolatey
+choco install -y git delta bat fzf ripgrep fd wget unzip gzip mingw make
